ab4af60a7ced40677c9c53e28248e1a204b0325f
Amelie <amelieoller@gmail.com>
2020-01-07 15:51:22 -0800

    Create sinatra form lecture

---
+++ 13-sinatra-forms-and-rest/game-ratings/app/controllers/games_controller.rb
@@ -0,0 +1,22 @@
+class GamesController < ApplicationController
+
+	get '/games' do
+		@games = Game.all
+
+		erb :"games/index"
+	end
+
+	get '/games/new' do
+		erb :"games/new"
+	end
+
+	get '/games/:id' do
+		@game = Game.find(params[:id])
+
+		erb :"games/show"
+	end
+
+	post '/games' do
+		Game.create(params)
+	end
+end
