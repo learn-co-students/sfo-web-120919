484c9983834442ca2de56c3f5c97a39db5305bd5
Amelie <amelieoller@gmail.com>
2020-01-09 14:34:04 -0800

    Create intro to rails lecture

---
+++ 15-intro-rails/GOALS.md
@@ -0,0 +1,6 @@
+## Goals 🏔
+
+- [x] 🌪 Take tour of Rails app generation _(A blog in 10 mins)_
+- [x] ⚖️ Compare Sinatra and Rails
+- [x] 🗺 Review past, present, future of Rails
+- [x] 🛠 Build out Rails app with M, V, and C
---
+++ 15-intro-rails/petstore/db/seeds.rb
@@ -0,0 +1,12 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.create(name: "Potato", species: "Pig", num_legs: 3)
+Pet.create(name: "Bob", species: "Fish", num_legs: 0)
+Pet.create(name: "Fido", species: "Dog", num_legs: 4)
+Pet.create(name: "Arthur", species: "Ardvark", num_legs: 20)
