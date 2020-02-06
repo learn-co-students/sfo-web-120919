dfefdf434003d24615ef5bf4087f093c20ea9b5d
Amelie <amelieoller@gmail.com>
2020-02-06 11:43:28 -0800

    Create js functional programming lecture

---
+++ 31-functional-programming/index.js
@@ -0,0 +1,13 @@
+(function(){
+
+	console.log("Let's do some work here!")
+
+	const myContainer = document.getElementById('container')
+	const another = document.getElementById('container')
+	
+	function anotherFunction() {
+		console.log(myContainer)
+	}
+
+})()
+
